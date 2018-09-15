dede5901cd3ff9f7360ddd15d6326067d181d2e0
Judiah McRoberts <judiahm@gmail.com>
2018-09-14 21:19:25 -1000

    Checkpoint Bloc-Jams_React Player-bar Buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums.js';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component{
     constructor(props) {
@@ -70,6 +71,14 @@ class Album extends Component{
         }
      }
 
+     handlePreviousClick() {
+         const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+         const newIndex = Math.max(0, currentIndex - 1);
+         const newSong = this.state.album.songs[newIndex];
+         this.setSong(newSong);
+         this.play();
+     }
+
     render() {
         return (
           <section className="album">
@@ -99,6 +108,12 @@ class Album extends Component{
            }
            </tbody>
            </table>
+           <PlayerBar 
+                isPlaying={this.state.isPlaying}
+                currentSong={this.state.currentSong}
+                handleSongClick={() => {this.handleSongClick(this.state.currentSong)}} 
+                handlePrevClick={() => this.handlePreviousClick()}
+                />
         </section>
         );
     }
